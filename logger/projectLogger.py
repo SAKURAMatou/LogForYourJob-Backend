fb4fad03ec3e8e00d0cf95bde8f43ac2b1360198
“DML” <dailin1357@gmail.com>
2023-08-07 21:04:37 +0800

    工程添加统一的日志功能，并且把fastapi ，uvicorn的日志指向统一的日志工具

---
+++ logger/projectLogger.py
@@ -0,0 +1,25 @@
+import os
+
+from loguru import logger
+
+current_file_path = os.path.abspath(__file__)
+# 循环回溯到工程的根目录（这假设你的根目录有一个特定的标识，比如 '.env' 文件或其他）
+while not os.path.exists(os.path.join(current_file_path, '.root')):
+    current_file_path = os.path.dirname(current_file_path)
+project_root = current_file_path
+
+# 指定 `applog` 文件夹路径
+applog_folder = os.path.join(project_root, "applog")
+# 如果文件夹不存在，创建它
+if not os.path.exists(applog_folder):
+    os.makedirs(applog_folder)
+
+# 设置日志文件路径
+
+
+log_file_path = os.path.join(applog_folder, 'logger_{time:YYYY_MM_DD}.log')
+# serialize=True:日志在传递给loguru的日志接收器之前都会序列化为json,包含很多线程等信息
+log_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | {level} |  <cyan>{module}:{function}:{line}</cyan> - <level>{message}</level>"
+
+logger.add(log_file_path, format=log_format, rotation='100 MB',
+           backtrace=True, level="INFO")
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import sys
+
 import uvicorn
+import logging
 from fastapi import FastAPI, Request
 
 from login import routers as login
@@ -7,7 +10,7 @@ from logforjob import jobSearchRouters
 from dao.database import Base, engine
 from config import get_settings
 from utils.JWTUtil import refresh_token
-from logforjob.models import JobSearch
+from logger.projectLogger import logger
 
 app = FastAPI()
 
@@ -31,12 +34,47 @@ app.include_router(tokenRouter.router)
 app.include_router(login.router)
 app.include_router(jobSearchRouters.router)
 
+from loguru import logger
+
+
+# 把fastapi、uvicorn的日志拦截并交给Loguru进入打印
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+logging.basicConfig(handlers=[InterceptHandler()], level=0)
+
+# Redirect Uvicorn logs
+for name in ("uvicorn", "uvicorn.error", "fastapi"):
+    logging_logger = logging.getLogger(name)
+    logging_logger.handlers = [InterceptHandler()]
+
 
 @app.get('/refresh')
 async def refresh():
     """发现uvicorn的热部署需要请求一次接口才能完全热部署完成，"""
     pass
 
+
+def setup_logging():
+    """logurn的配置函数"""
+    pass
+
+
+setup_logging()
 
 # //根据模型创建对饮的数据表
 Base.metadata.create_all(bind=engine)
