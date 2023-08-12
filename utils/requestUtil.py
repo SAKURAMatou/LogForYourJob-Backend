90bf737335ed8d380af1f5d157a2ac3dfb25270c
“DML” <dailin1357@gmail.com>
2023-08-12 12:25:51 +0800

    响应的失败函数错误处理

--- utils/requestUtil.py
+++ utils/requestUtil.py
@@ -1,6 +1,6 @@
 class Response:
     def fail(self, code: int, msg: str, custom: dict = {}) -> dict:
-        return {'state': {"code": '200', "msg": msg}, "custom": custom}
+        return {'state': {"code": code, "msg": msg}, "custom": custom}
 
     def success(self, msg: str, custom: dict = {}) -> dict:
         return {'state': {"code": '200', "msg": msg}, "custom": custom}
